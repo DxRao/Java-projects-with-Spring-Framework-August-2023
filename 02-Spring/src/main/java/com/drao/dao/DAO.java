fb768fc570075614361b58af1e371c0736af321b
D Rao <tdayakarrao@gmail.com>
2023-08-15 23:30:30 +0530

    02 - Spring (with Annotation approach)

---
+++ 02-Spring/src/main/java/com/drao/dao/DAO.java
@@ -0,0 +1,14 @@
+package com.drao.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DAO {
+	
+	public DAO(){
+		
+		System.out.println("DAO obj is being created");
+		
+	}
+
+}
